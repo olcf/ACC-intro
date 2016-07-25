4a131e71062a226101bf4b5e73e28ca799eadb94
Veronica Vergara L <verolero86@gmail.com>
2016-07-24 22:13:12 -0400

    updated omp4 vecAdd

--- Vec/vec_parallel_omp4_solution.c
+++ Vec/vec_parallel_omp4_solution.c
@@ -29,18 +29,17 @@ int main( int argc, char* argv[] ) {
     a = (double*)malloc(bytes);
     b = (double*)malloc(bytes);
     c = (double*)malloc(bytes);
-    #pragma omp target data alloc(a[0:n], b[0:n], c[0:n]) 
+    #pragma omp target data map(alloc:a[0:n],b[0:n],c[0:n]) map(from:c)
+    {
 
     // Initialize content of input vectors, vector a[i] = sin(i)^2 vector b[i] = cos(i)^2
     int i;
-    #pragma omp target
+    #pragma omp target 
     #pragma omp parallel for
     for(i=0; i<n; i++) {
         a[i] = sin_squared(i/M_PI);;
         b[i] = cos_squared(i/M_PI);
     }   
-    #pragma omp end parallel for
-    #pragma omp end target 
 
     // sum component wise and save result into vector c
     #pragma omp target
@@ -48,8 +47,6 @@ int main( int argc, char* argv[] ) {
     for(i=0; i<n; i++) {
         c[i] = a[i] + b[i];
     }
-    #pragma omp end parallel for
-    #pragma omp end target
  
     // Sum up vector c and print result divided by n, this should equal 1 within error
     double sum = 0.0;
@@ -58,17 +55,16 @@ int main( int argc, char* argv[] ) {
     for(i=0; i<n; i++) {
         sum += c[i];
     }
-    #pragma omp end parallel for
-    #pragma omp end target
-
+    
+    
     sum /= n;
     if(sum - 1.0 < 0.0000001)
-      printf("result correct\n");
+      printf("result correct: %f\n",sum);
     else
       printf("result incorrect!\n");
  
+    }
     // Release memory
-    #pragma omp end target data 
     free(a);
     free(b);
     free(c);

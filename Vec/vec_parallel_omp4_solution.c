cc91c0d9cacc6bdb064a04652b2b108a135da217
Veronica Vergara L <verolero86@gmail.com>
2017-02-27 19:44:36 -0500

    add teams target

--- Vec/vec_parallel_omp4_solution.c
+++ Vec/vec_parallel_omp4_solution.c
@@ -15,6 +15,8 @@ int main( int argc, char* argv[] ) {
  
     // Size of vectors
     int n = 1000000;
+    int teams = 0;
+    int threads = 0;
  
     // Input vectors
     double *restrict a;
@@ -25,6 +27,14 @@ int main( int argc, char* argv[] ) {
     // Size, in bytes, of each vector
     size_t bytes = n*sizeof(double);
  
+    #pragma omp target teams
+    {
+        threads = omp_get_num_threads();
+        teams = omp_get_num_teams();
+    }
+
+    printf("Teams = %d, Threads = %d \n",teams,threads);
+
     // Allocate memory for each vector
     a = (double*)malloc(bytes);
     b = (double*)malloc(bytes);
@@ -34,24 +44,21 @@ int main( int argc, char* argv[] ) {
 
     // Initialize content of input vectors, vector a[i] = sin(i)^2 vector b[i] = cos(i)^2
     int i;
-    #pragma omp target 
-    #pragma omp parallel for
+    #pragma omp target teams distribute parallel for
     for(i=0; i<n; i++) {
         a[i] = sin_squared(i/M_PI);;
         b[i] = cos_squared(i/M_PI);
     }   
 
     // sum component wise and save result into vector c
-    #pragma omp target
-    #pragma omp parallel for 
+    #pragma omp target teams distribute parallel for
     for(i=0; i<n; i++) {
         c[i] = a[i] + b[i];
     }
  
     // Sum up vector c and print result divided by n, this should equal 1 within error
     double sum = 0.0;
-    #pragma omp target
-    #pragma omp parallel for reduction(+:sum)
+    #pragma omp target teams distribute parallel for reduction(+:sum)
     for(i=0; i<n; i++) {
         sum += c[i];
     }
